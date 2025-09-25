70759f3e34eeee01410f87e4fca64a41853dad53
shantelle wambui <shantellewambuikungu@gmail.com>
2025-09-25 12:42:51 +0300

    Create ProgressForm.jsx with basic form structure

---
+++ clients/src/components/ProgressForm.jsx
@@ -0,0 +1,11 @@
+function ProgressForm() {
+  return (
+    <form>
+      <input type="text" placeholder="Log your progress..." />
+      <button type="submit">Add</button>
+    </form>
+  );
+}
+
+export default ProgressForm;
+// first i create a form component with an input and button
