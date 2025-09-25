736c5d1a372b470687b60fc2cbf9ddd4d50ea4cd
shantelle wambui <shantellewambuikungu@gmail.com>
2025-09-25 12:49:32 +0300

    Add useState to handle note input

--- clients/src/components/ProgressForm.jsx
+++ clients/src/components/ProgressForm.jsx
@@ -1,7 +1,17 @@
+import { useState } from "react";
+
+//import usestate to keep track of input value
+
 function ProgressForm() {
+    const [note, setNote] = useState("");
+
   return (
     <form>
-      <input type="text" placeholder="Log your progress..." />
+      <input type="text" 
+      placeholder="Log your progress..."
+      value={note}
+      onChange={(e) => setNote(e.target.value)} 
+      />
       <button type="submit">Add</button>
     </form>
   );
