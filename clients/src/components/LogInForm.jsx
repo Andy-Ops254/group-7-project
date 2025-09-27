a2e6142275620cb5e31fa62eeadb872fd750ed00
shantelle wambui <shantellewambuikungu@gmail.com>
2025-09-27 18:12:43 +0300

    feat: add useState for login form data

--- clients/src/components/LogInForm.jsx
+++ clients/src/components/LogInForm.jsx
@@ -5,6 +5,19 @@ import { useNavigate } from "react-router-dom";
 function LogInForm() {
     const navigate = useNavigate();
 
+    const [logData, setLogData] = useState({
+    name: "",
+    email: "",
+  });
+  //then i usestate to store form inputs that is name and email
+
+  function handleChange(e){
+    const { name, value } = e.target;
+    setLogData({ ...logData, [name]: value });
+    
+  }
+
+
   return (
     <div>
       <p>works</p>
