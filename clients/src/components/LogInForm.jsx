6257df1d6bfc0862ff78b0deab493eeed30a8b60
shantelle wambui <shantellewambuikungu@gmail.com>
2025-09-28 08:13:05 +0300

    feat: implement handleSubmit with fetch to backend

--- clients/src/components/LogInForm.jsx
+++ clients/src/components/LogInForm.jsx
@@ -16,11 +16,36 @@ function LogInForm() {
     setLogData({ ...logData, [name]: value });
 
   }
+    function handleSubmit(e) {
+    e.preventDefault(); // prevent page reload
+
+    fetch("http://127.0.0.1:5555/login", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(logData),
+    })
+      .then((res) => {
+        if (res.status === 404) {
+          navigate("/register");//if user not found  go to register page
+          return;
+        }
+        if (!res.ok) {
+          throw new Error("Invalid login");
+        }
+        return res.json();
+      })
+      .then((response) => {
+        console.log("Login success:", response);
+        navigate("/home"); // redirect to home
+        setLogData({ name: "", email: "" });
+      });
+  }
+
 
 
   return (
     <div>
-      <form>
+      <form onSubmit={handleSubmit}>
         <input
         name ="name"
         type="text"
