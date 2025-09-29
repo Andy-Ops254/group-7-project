24140ebe4a4f1ca5a15e53d3e6d1de0ac34281c1
Andy-Ops254 <ghostandy254@outlook.com>
2025-09-29 23:45:18 +0300

    modify start.sh

--- start.sh
+++ start.sh
@@ -3,9 +3,9 @@
 # Exit on error
 set -e
 
-# Install Node modules and build React
-npm install --prefix clients
-npm run build --prefix clients
+# # Install Node modules and build React
+# npm install --prefix clients
+# npm run build --prefix clients
 
 # Run the Flask app with gunicorn
 exec gunicorn app:app --bind 0.0.0.0:$PORT
