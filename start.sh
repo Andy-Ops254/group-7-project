b92661e0acc464906fba05085d12d6754d38d6d6
Andy-Ops254 <ghostandy254@outlook.com>
2025-09-29 23:54:01 +0300

    modify start.sh

--- start.sh
+++ start.sh
@@ -7,6 +7,10 @@ set -e
 # npm install --prefix clients
 # npm run build --prefix clients
 
+
+# Change to server directory where app.py is located
+cd server
+
 # Run the Flask app with gunicorn
 exec gunicorn app:app --bind 0.0.0.0:$PORT
 
