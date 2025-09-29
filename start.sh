7ca85f64f27ddb5165d070bd93761c5359c18ba4
Andy-Ops254 <ghostandy254@outlook.com>
2025-09-29 15:56:18 +0300

    Update start.sh with React build commands

--- start.sh
+++ start.sh
@@ -1,2 +1,12 @@
 #!/bin/bash
-gunicorn app:app --bind 0.0.0.0:$PORT
+
+# Exit on error
+set -e
+
+# Install Node modules and build React
+npm install --prefix clients
+npm run build --prefix clients
+
+# Run the Flask app with gunicorn
+exec gunicorn app:app --bind 0.0.0.0:$PORT
+
